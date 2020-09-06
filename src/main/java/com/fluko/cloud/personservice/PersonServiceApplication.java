9de778d1fff8dbdfa82ec01779b4c523167f42ec
Gerardo RAZO JABANA <slaarks.com>
2020-09-05 19:57:41 -0500

    Handle correlation IDs.

--- src/main/java/com/fluko/cloud/personservice/PersonServiceApplication.java
+++ src/main/java/com/fluko/cloud/personservice/PersonServiceApplication.java
@@ -1,13 +1,41 @@
 package com.fluko.cloud.personservice;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import com.fluko.cloud.personservice.util.UserContextInterceptor;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PersonServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PersonServiceApplication.class, args);
 	}
+	
+    @LoadBalanced
+    @Bean
+    public RestTemplate getRestTemplate(){
+        RestTemplate template = new RestTemplate();
+        List interceptors = template.getInterceptors();
+        if (interceptors==null){
+            template.setInterceptors(Collections.singletonList(new UserContextInterceptor()));
+        }
+        else{
+            interceptors.add(new UserContextInterceptor());
+            template.setInterceptors(interceptors);
+        }
+
+        return template;
+    }
+
 
 }
